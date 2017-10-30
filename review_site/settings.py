34b07349b2159d4313a7755fe68d9da3ad2538bc
atleyvarghese <atleyvarghese@gmail.com>
2017-10-30 15:02:43 +0530

    heroku 4

--- review_site/settings.py
+++ review_site/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION = 'review_site.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-#
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators

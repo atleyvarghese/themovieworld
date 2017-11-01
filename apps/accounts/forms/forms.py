0142b882243b0c43dc104a3ff32c12b19c33c97b
atleyvarghese <atleyvarghese@gmail.com>
2017-11-01 18:39:39 +0530

    changes to code 2

--- apps/accounts/forms/forms.py
+++ apps/accounts/forms/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class editform(forms.Form):
     username = forms.CharField(max_length=100)
-    lastname = forms.CharField(max_length=100)
-    firstname = forms.CharField(max_length=100)
+    lastname = forms.CharField(max_length=100,error_messages={'required': 'Please enter your Lastname'})
+    firstname = forms.CharField(max_length=100,error_messages={'required': 'Please enter your Firstname'})
     email = forms.CharField(max_length=100)
